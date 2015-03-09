2311bf3798fd4f0a44a36c473413a4993318f35f
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-09 14:12:13 +0700

    Create MessageTemplatePage.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/pages/MessageTemplatePage.java
@@ -0,0 +1,20 @@
+package com.softhaxi.shortsage.v1.pages;
+
+import java.swing.*;
+
+/**
+* @author Ivo Hutasoit
+* @since 1
+* @version 1.0.0
+*/
+public class MessageTemplatePage extends JPanel {
+  /**
+   * Constructor
+   */
+  public ContactPersonPage() {
+    initComponents();
+  }
+  
+  private void initComponents() {
+  }
+}
