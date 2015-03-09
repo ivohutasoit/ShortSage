ea68c67825253e750f085174be709c76fabd038d
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-09 14:10:44 +0700

    Create ContactGroupPage.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/pages/ContactGroupPage.java
@@ -0,0 +1,22 @@
+package com.softhaxi.shortsage.v1.pages
+
+import java.swing.*;
+
+/**
+ * @author Ivo Hutasoit
+ * @since 1
+ * @version 1.0.0
+ */
+public class ContactGroupPage extends JPanel {
+    /**
+     * Constructor
+     */
+    public ContactPersonPage() {
+        initComponents();
+    }
+    
+    
+    private void initComponents() {
+      
+    }
+}
