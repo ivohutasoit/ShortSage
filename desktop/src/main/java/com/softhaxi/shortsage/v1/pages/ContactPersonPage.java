292f7b94b381af9abe5e076f0308d91b06071731
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-09 13:22:54 +0700

    Create ContactPersonPage.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/pages/ContactPersonPage.java
@@ -0,0 +1,22 @@
+package com.softhaxi.shortsage.v1.pages
+
+import java.swing.*;
+
+/**
+ * @author Ivo Hutasoit
+ * @since 1
+ * @version 1.0.0
+ */
+public class ContactPersonPage extends JPanel {
+    /**
+     * Constructor
+     */
+    public ContactPersonPage() {
+        initComponents();
+    }
+    
+    
+    private void initComponents() {
+      
+    }
+}
