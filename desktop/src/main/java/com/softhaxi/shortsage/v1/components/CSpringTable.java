3f5316ee8c2627b082fddf179519fbc933f3ab58
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-10 13:54:58 +0700

    Create CSpringTable.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/components/CSpringTable.java
@@ -0,0 +1,35 @@
+package com.softhaxi.shortsage.v1.components;
+
+/**
+ *
+ * Link Reference <a href="http://stackoverflow.com/questions/15126651/how-to-auto-resize-all-columns-of-a-jtable-to-the-same-size">Same Width Column</a>
+ */
+public class CSpringTable extends JTable {
+    
+    public CSpringTable() {
+        setAutoResizeMode(AUTO_RESIZE_OFF);
+    }
+    
+    public CSpringTable(TableModel model) {
+        super(model);
+        setAutoResizeMode(AUTO_RESIZE_OFF);
+    }
+    
+    @Override
+    public void doLayout() {
+        int ww = getWidth();
+        int cc = getColumnCount();
+        int cs = ww / cc;
+        for (int ii = 0; ii < cc; ii++) {
+            TableColumn clmn = getColumnModel().getColumn(ii);
+            clmn.setResizable(false);
+            clmn.setPreferredWidth(ww);
+        }
+        super.doLayout();
+    }
+    
+    @Override
+    public boolean getScrollableTracksViewportWidth() {
+        return true;
+    }
+}
