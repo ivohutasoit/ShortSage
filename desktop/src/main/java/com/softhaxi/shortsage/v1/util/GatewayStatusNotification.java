c9429dc3a90c8a39a507d3912876951a9d1324bf
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-11 12:13:55 +0700

    Create GatewayStatusNotification.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/util/GatewayStatusNotification.java
@@ -0,0 +1,9 @@
+package com.softhaxi.shortsage.v1.modem.impl;
+
+
+public class GatewayStatusNotification implements IGatewayStatusNotification {
+    public void process(AGateway gateway, GatewayStatuses oldStatus, GatewayStatuses newStatus)
+    {
+      System.out.println(">>> Gateway Status change for " + gateway.getGatewayId() + ", OLD: " + oldStatus + " -> NEW: " + newStatus);
+    }
+}
