edb1db09ae88a3c5f99832a82c742bc5ac292126
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-11 08:45:46 +0700

    Update OutboxPage.java

--- desktop/src/main/java/com/softhaxi/shortsage/v1/pages/OutboxPage.java
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/pages/OutboxPage.java
@@ -13,5 +13,36 @@ public class OutboxPage extends JPanel {
      * 
      */
     public OutboxPage() {
+        initComponents();
+    }
+    
+    private void initComponents() {
+        setLayout(new BorderLayout(0, 4));
+        setBorder(new EmptyBorder(4, 4, 4, 4));
+        
+        JPanel pUtil = new JPanel(new GridLayout(1, 2));
+        pUtil.add(new CSearchField() {
+            
+            @Override
+            public void doSearch() {
+                JOptionPane.showMessageDialog(null, "Search action was not implemented full!");
+            }
+        });
+        
+        add(pUtil, BorderLayout.NORTH);
+        
+        pDetail = new JPanel(new BorderLayout(0, 3));
+        JToolBar tpDetail = new JToolBar();
+        tpDetail.setBorder(new CompoundBorder(new EtchedBorder(), new EmptyBorder(2, 2, 2, 2)));
+        tpDetail.setFloatable(false);
+        tpDetail.add(new JButton("New", new ImageIcon(getClass().getClassLoader().getResource("images/ic_new.png"))));
+        tpDetail.add(new JButton("Edit", new ImageIcon(getClass().getClassLoader().getResource("images/ic_edit.png"))));
+        tpDetail.add(new JToolBar.Separator());
+        tpDetail.add(new JButton(new ImageIcon(getClass().getClassLoader().getResource("images/ic_delete.png"))));
+        tpDetail.add(Box.createHorizontalGlue());
+        tpDetail.add(new JButton(new ImageIcon(getClass().getClassLoader().getResource("images/ic_refresh.png"))));
+        
+        pDetail.add(tpDetail, BorderLayout.NORTH);
+        add(pDetail, BorderLayout.CENTER);
     }
 }
