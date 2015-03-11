113302846da9af3e9426d4c66b3d04940f82952b
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-11 08:24:08 +0700

    Create CDialog.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/dialogs/CDialog.java
@@ -0,0 +1,21 @@
+package com.softhaxi.shortsage.v1.dialogs;
+
+import com.softhaxi.shortsage.v1.intfs.IPanelDialog;
+
+/**
+ * Implementation a generic dialog
+ *
+ * @author Ivo Hutasoit
+ * @since 1
+ * @version 1.0.0
+ */
+public class CDialog extends JDialog {
+    
+    public CDialog(JFrame parent, IPanelDialog panel, String title, boolean modal) {
+        super(parent, title, modal);
+        add((JPanel) panel);
+        panel.setDialogHost(this);
+        pack();
+        setLocationRelativeTo(parent);
+    }
+}
