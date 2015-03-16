23da4fa405b0ca67794d74d25a58598bf8be8452
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-16 13:38:16 +0700

    Create Message.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/dto/Message.java
@@ -0,0 +1,45 @@
+package com.softhaxi.shortsage.v1.dto;
+
+@Entity
+@Table(name = "T0MSSG")
+public class Message implements Serializable {
+  @Id
+  @GeneratedValue(generator="system-uuid")
+  @GenericGenerator(name="system-uuid", strategy = "uuid")
+  @Column(name = "MSSGID", unique = true)
+  private String id;
+  
+  @Column(name = "MSSGCT", length = 100)
+  private String contact;
+  
+  @Column(name = "MSSGGR", length = 100)
+  private String group;
+  
+  @Column(name = "MSTEXT")
+  private String message;
+  
+  @Column(name = ""MSDATE)
+  private Date date;
+  
+  @Column(name = "MSFLDR", length = 100)
+  private String folder;
+  
+  @Column(name = "MSRCST")
+  private int status;
+  
+  @Column(name = "MSCRON")
+  private Date createdOn;
+  
+  @Column(name = "MSCRBY", length = 100)
+  private String createdBy;
+  
+  private Date modifiedOn;
+  private String modifiedBy;
+  
+  @Column(name = "MSDLST")
+  public int deleteState;
+  
+  @Version
+  @Column(name="MGVRSN")
+  private Integer version;
+}
