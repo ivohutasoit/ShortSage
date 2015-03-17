66c0f6b16bc6c4405b65eea7c57ff71de00f92c4
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-17 15:43:18 +0700

    Create CPanel.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/components/CPanel.java
@@ -0,0 +1,10 @@
+package com.softhaxi.shortsage.v1.components;
+
+public abstract class CPanel extends JPanel {
+    protected CPanel() {
+      
+    }
+    
+    protected abstract void initComponents();
+    protected abstract void initData();
+}
