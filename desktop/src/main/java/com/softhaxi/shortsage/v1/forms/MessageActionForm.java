a7646877ca799358207793d54c9a53be985e43bc
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-17 11:45:46 +0700

    Update MessageActionForm.java

--- desktop/src/main/java/com/softhaxi/shortsage/v1/forms/MessageActionForm.java
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/forms/MessageActionForm.java
@@ -4,13 +4,39 @@ import com.softhaxi.shortsage.v1.dto.Message;
 
 public class MessageActionForm extends ActionForm<Message> {
     
+    private JTextField tContact;
+    private JCalendarComboBox cDate;
+    private JTextArea tText;
+    private JComboBox cStatus;
+    private JComboBox cHandle;
     
+    public MessageActionForm() {
+        super();
+    }
     
+    public MessageActionForm(Message object) {
+        super(object);
+    }
     
+    public MessageActionForm(ActionState state, Message object) {
+        super(state, object)
+    }
     
     @Override
-    public void initData() {
+    private void initComponents() {
+        
+    }
     
+    @Override
+    private void initState() {
+        
+    }
+    
+    @Override
+    private void initData() {
+        if(object != null) {
+            
+        }
     }
   
 }
