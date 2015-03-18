4f9f2a11049e3a6750fa5cef032e9724b8b1f6d0
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-18 11:43:23 +0700

    Create SystemProgram.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/dto/SystemProgram.java
@@ -0,0 +1,15 @@
+package com.softhaxi.shortsage.v1.dto;
+
+public class SystemProgram implements Serializable {
+  private String id;
+  private String code;
+  private String name;
+  private String program;
+  private String library;
+  private int status;
+  private Date createdOn;
+  private String createdBy;
+  private Date modifiedOn;
+  private String modifiedBy;
+  private int deletedState;
+}
