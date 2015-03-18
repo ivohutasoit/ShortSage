afecdf2e8c207573074c24f0f508809eaeb9aa34
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-18 09:00:36 +0700

    Call initComponents and initData on Constructor

--- desktop/src/main/java/com/softhaxi/shortsage/v1/components/CPanel.java
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/components/CPanel.java
@@ -4,7 +4,9 @@ import javax.swing.JPanel;
 
 public abstract class CPanel extends JPanel {
     public CPanel() {
-      
+      super();
+      initComponents();
+      initData();
     }
     
     public abstract void initComponents();
