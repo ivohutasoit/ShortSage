1121ddb18209d2020abe38feb8ab73a16ee25656
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-18 08:26:56 +0700

    Implement initComponents function

--- desktop/src/main/java/com/softhaxi/shortsage/v1/forms/MessageActionForm.java
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/forms/MessageActionForm.java
@@ -10,11 +10,33 @@ import javax.swing.JTextField;
 
 public class MessageActionForm extends CActionForm<Message> {
     
+    private final static String[] STATUS_LIST = {
+      "CREATED",
+      "DRAFT",
+      "SENT",
+      "FAILED",
+      "OBSOLETED",
+      "UNREAD",
+      "READ",
+      "REPLIED"
+    };
+    
+    private final static String[] HANDLE_LIST = {
+       "CREATE",
+       "SEND IMMEDIATELY",
+       "RESEND",
+       "UNSEND"
+    } ;
+    
+    private JRadioButton rImmidiate;
+    private JRadioButton rSchedule;
     private JTextField tContact;
     private JCalendar cDate;
     private JTextArea tText;
-    private JComboBox cStatus;
-    private JComboBox cHandle;
+    private JComboBox<String> cStatus;
+    private JComboBox<String> cHandler;
+    
+    private JButton bReply;
     
     public MessageActionForm() {
         super();
@@ -31,11 +53,76 @@ public class MessageActionForm extends CActionForm<Message> {
     @Override
     public void initComponents() {
       super.initComponents();
+      setPreferredSize(new Dimension(500, 600));
+      
+      bReply = new JButton("Reply");
+      tBar.add(tReply, 4);
+      
+      JPanel pForm = new JPanel();
+      GroupLayout lForm = new GroupLayout(pForm);
+      pForm.setLayout(lForm);
+      lForm.setAutoCreateGaps(true);
+      lForm.setAutoCreateContainerGaps(true);
+     
+      JLabel lContact = new JLabel("Contact:");
+      tContact = new JTextField();
+      
+      JLabel lDate = new JLabel("Transaction Date:");
+      cDate = new JCalendar();
+      
+      JLabel lStatus = new JLabel("Message Status:");
+      cStatus = new JComboBox(STATUS_LIST);
+      
+      JLabel lHandler = new JLabel("Handler:");
+      cHandler = new JComboBox(HANDLE_STATUS);
+      
+      JLabel lText = new JLabel("Message:");
+      tText = new JTextArea();
+      
+      lForm.setHorizontalGroup(lForm.createSequentialGroup()
+        .addGroup(lForm.createParallelGroup(GroupLayout.Alignment.LEADING)
+        .addComponent(lContact)
+        .addComponent(lDate)
+        .addComponent(lStatus)
+        .addComponent(lHandler)
+        .addComponent(lText))
+        .addGroup(lForm.createParallelGroup(GroupLayout.Alignment.LEADING)
+        .addComponent(tContact)
+        .addComponent(cDate)
+        .addComponent(cStatus)
+        .addComponent(cHandler)
+        .addComponent(tText))
+        );
+     lForm.setVerticalGroup(lForm.createSequentialGroup()
+        .addGroup(lForm.createParallelGroup(GroupLayout.Alignment.BASELINE)
+        .addComponent(lContact)
+        .addComponent(tContact))
+        .addGroup(lForm.createParallelGroup(GroupLayout.Alignment.BASELINE)
+        .addComponent(lDate)
+        .addComponent(cDate))
+        .addGroup(lForm.createParallelGroup(GroupLayout.Alignment.BASELINE)
+        .addComponent(lStatus)
+        .addComponent(cStatus))
+        .addGroup(lForm.createParallelGroup(GroupLayout.Alignment.BASELINE)
+        .addComponent(lHandler)
+        .addComponent(cHandler))
+        .addGroup(lForm.createParallelGroup(GroupLayout.Alignment.BASELINE)
+        .addComponent(lText)
+        .addComponent(tText))
+        );
+        add(pForm, BorderLayout.CENTER);
     }
     
     @Override
     public void initState() {
       super.initState();
+      
+      if(!object.getFolder().equalsIgnoreCase("INBOX") && 
+        (state == ActionState.CREATE || state == ActionState.EDIT)) {
+          bReply.setVisible(false);
+      } else {
+          bReply.setVisible(true);
+      }
     }
     
     @Override
