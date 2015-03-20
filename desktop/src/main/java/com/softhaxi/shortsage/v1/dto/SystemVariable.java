fd2c55a62a960c7afaa5d4a65c513183ce845bb0
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-20 13:49:07 +0700

    Create SystemVariable.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/dto/SystemVariable.java
@@ -0,0 +1,23 @@
+package com.softhaxi.shortsage.v1.dto;
+
+public class SystemVariable implements Serializable {
+  private String id;
+  
+  private String name;
+  
+  private String value;
+  
+  private String param;
+  
+  private int status;
+  
+  private String createdBy;
+  
+  private Date createdOn;
+  
+  private String modifiedBy;
+  
+  private String modifiedOn;
+  
+  private int deletionState;
+}
