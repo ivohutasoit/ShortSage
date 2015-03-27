df330b52f5dc67a2acdb07b63d1773123b4d07bb
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-27 12:22:24 +0700

    Create OptionDialog.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/dialog/OptionDialog.java
@@ -0,0 +1,12 @@
+package com.softhaxi.shortsage.v1.dialog;
+
+/** 
+ * Configure option and preferences of whole system. User with system administrator 
+ * can add, edit, and delete also configure menu of whole system for current user.
+ *
+ * @author Ivo Hutasoit
+ * @since 1
+ * @version 1.0.0
+ */
+public class OptionDialog extends JDialog {
+}
