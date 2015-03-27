9c0a2402d265226657f1b912e6e41c8bf6131d4f
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-27 14:31:28 +0700

    Create CTitledPanel.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/component/CTitledPanel.java
@@ -0,0 +1,15 @@
+package com.softhaxi.shortsage.v1.component;
+
+/**
+ * A simple JPanel with a border and a title
+ * Reference 
+ * <a href="http://www.java2s.com/Code/Java/Swing-Components/AsimpleJPanelwithaborderandatitle.htm">Simple Title Panel</a>
+ * <a hreg="http://stackoverflow.com/questions/19172982/java-swing-programming-panel-title">Panel Title</a>
+ *
+ * @author Ivo Hutasoit
+ * @since 1
+ * @version 1.0.0
+ */
+public class CTitledPanel extends JPanel {
+  
+}
