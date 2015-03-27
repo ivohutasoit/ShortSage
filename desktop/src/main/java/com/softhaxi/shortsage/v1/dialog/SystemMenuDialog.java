ac1c177a10afe589c8d0452d5b9d7162efd6eb3b
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-27 12:18:47 +0700

    Create SystemMenuDialog.java

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/dialog/SystemMenuDialog.java
@@ -0,0 +1,11 @@
+package com.softhaxi.shortsage.v1.dialog;
+
+/**
+ * Configuration of tree menu on window on menubar and toolbar.
+ *
+ * @author Ivo Hutasoit
+ * @since 1
+ * @version 1.0.0
+ */
+public class SystemMenuDialog extends JDialog {
+}
