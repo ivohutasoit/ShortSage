effcee43356430bdd375516663c84f1d07b014de
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-03-27 12:41:07 +0700

    Update CPanel.java

--- desktop/src/main/java/com/softhaxi/shortsage/v1/component/CPanel.java
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/component/CPanel.java
@@ -3,6 +3,8 @@ package com.softhaxi.shortsage.v1.component;
 import javax.swing.JPanel;
 
 public abstract class CPanel extends JPanel {
+    private IHost host;
+    
     public CPanel() {
       super();
       initComponents();
