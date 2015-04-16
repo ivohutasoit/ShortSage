cef32346faf2b14a60d796b0a4f8ad6c2e0e32bb
Ivo Hutasoit <raja.yudhaps@gmail.com>
2015-04-17 06:45:46 +0700

    Fix Basic Entity

--- desktop/src/main/java/com/softhaxi/shortsage/v1/dto/BasicEntity.java
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/dto/BasicEntity.java
@@ -1,8 +1,17 @@
 package com.softhaxi.shortsage.v1.dto;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+import org.hibernate.annotations.GenericGenerator;
 
 /**
- * References 
+ * References
  * <ul>
  * <li>http://www.concretepage.com/hibernate/example-mappedsuperclass-hibernate</li>
  * <li>http://stackoverflow.com/questions/20063330/how-to-load-hibernate-cfg-xml-from-different-location</li>
@@ -14,33 +23,187 @@ package com.softhaxi.shortsage.v1.dto;
  */
 @MappedSuperclass
 public class BasicEntity implements Serializable {
-  @Id
-  @GeneratedValue(generator = "system-uuid")
-  @GenericGenerator(name = "system-uuid", strategy = "uuid")
-  protected String id;
-  
-  protected String name;
-  
-  protected String remark;
-  
-  protected int stateCode;
-  
-  protected int statusCode;
-  
-  protected String createdBy;
-  
-  @Temporal(TemporalType.TIMESTAMP)
-  protected Date createdDate;
-  
-  protected String modifiedBy;
-  
-  @Temporal(TemporalType.TIMESTAMP)
-  protected Date modifiedDate;
-  
-  protected int deletedState;
-  
-  @Version
-  protected int version;
-  
-  
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+    private String name;
+
+    private String remark;
+
+    private int stateCode;
+
+    private int statusCode;
+
+    private String createdBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    private String modifiedBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modifiedDate;
+
+    private int deletedState;
+
+    @Version
+    private int version;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the remark
+     */
+    public String getRemark() {
+        return remark;
+    }
+
+    /**
+     * @param remark the remark to set
+     */
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    /**
+     * @return the stateCode
+     */
+    public int getStateCode() {
+        return stateCode;
+    }
+
+    /**
+     * @param stateCode the stateCode to set
+     */
+    public void setStateCode(int stateCode) {
+        this.stateCode = stateCode;
+    }
+
+    /**
+     * @return the statusCode
+     */
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    /**
+     * @param statusCode the statusCode to set
+     */
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    /**
+     * @return the createdBy
+     */
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    /**
+     * @param createdBy the createdBy to set
+     */
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    /**
+     * @return the createdDate
+     */
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    /**
+     * @param createdDate the createdDate to set
+     */
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    /**
+     * @return the modifiedBy
+     */
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    /**
+     * @param modifiedBy the modifiedBy to set
+     */
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    /**
+     * @return the modifiedDate
+     */
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    /**
+     * @param modifiedDate the modifiedDate to set
+     */
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    /**
+     * @return the deletedState
+     */
+    public int getDeletedState() {
+        return deletedState;
+    }
+
+    /**
+     * @param deletedState the deletedState to set
+     */
+    public void setDeletedState(int deletedState) {
+        this.deletedState = deletedState;
+    }
+
+    /**
+     * @return the version
+     */
+    public int getVersion() {
+        return version;
+    }
+
+    /**
+     * @param version the version to set
+     */
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
 }
