5117139cdfd9f59041fdef102ce439bf3e969d7e
Ivo Hutasoit <raja.yudhaps@gmail.com>
2015-04-17 06:53:16 +0700

    Add InboundMessage Class

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/dto/InboundMessage.java
@@ -0,0 +1,20 @@
+package com.softhaxi.shortsage.v1.dto;
+
+import java.io.Serializable;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Ivo Hutasoit
+ * @since 1
+ * @version 1.0.0
+ */
+@Entity
+@Table(name = "T1INMG")
+@DiscriminatorValue("INBX")
+public class InboundMessage extends ModemMessage
+    implements Serializable {
+    
+}
