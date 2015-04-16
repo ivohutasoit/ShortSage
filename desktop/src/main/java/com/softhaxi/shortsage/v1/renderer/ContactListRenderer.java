dcc6f2396ff7a40f2612d2605984a7dd4639d63b
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-04-16 14:58:40 +0700

    Update ContactListRenderer.java

--- desktop/src/main/java/com/softhaxi/shortsage/v1/renderer/ContactListRenderer.java
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/renderer/ContactListRenderer.java
@@ -4,12 +4,14 @@ import javax.swing.DefaultListCellRenderer;
 
 /**
  * Render list of contacts
- * Reference <a href="http://stackoverflow.com/questions/22266506/how-to-add-image-in-jlist">List With Image</a>
- *
+ * Reference 
+ * <a href="http://stackoverflow.com/questions/22266506/how-to-add-image-in-jlist">List With Image</a>
+ * <a href="http://www.nguyenvanquan7826.com/2014/04/25/java-swing-tuy-bien-jlist-jlist-custom-renderer/?lang=en">Testing</a>
+ * 
  * @author Ivo Hutasoit
  * @since 1
  * @version 1.0.0
  */
-public class ContactListRenderer extends DefaultListCellRenderer {
+public class ContactListRenderer extends DefaultListCellRenderer<Contact> {
   
 }
