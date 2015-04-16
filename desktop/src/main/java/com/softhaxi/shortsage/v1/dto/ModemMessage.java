163cc3588a325ccc2a0e24ed4fc3d212bc8236c3
Ivo Hutasoit <raja.yudhaps@gmail.com>
2015-04-17 06:54:40 +0700

    Revert "Revert "Add InboundMessage Class""
    This reverts commit 17f3fc7b009f8e1b13aa636305885036e8416805.

--- desktop/src/main/java/com/softhaxi/shortsage/v1/dto/ModemMessage.java
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/dto/ModemMessage.java
@@ -1,7 +1,6 @@
 package com.softhaxi.shortsage.v1.dto;
 
 import java.io.Serializable;
-import java.util.Date;
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
@@ -9,12 +8,9 @@ import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 /**
  * References
@@ -25,14 +21,14 @@ import javax.persistence.TemporalType;
  */
 @Entity
 @Table(name = "T0MSSG")
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(
         name = "MGMGFD",
         discriminatorType = DiscriminatorType.STRING
 )
 @DiscriminatorValue(value = "MSSG")
 @AttributeOverrides({
-    @AttributeOverride(name = "id", 
+    @AttributeOverride(name = "id",
             column = @Column(name = "MGMGID", unique = true)),
     @AttributeOverride(name = "name",
             column = @Column(name = "MGMGNA", nullable = false, length = 100)),
@@ -58,12 +54,15 @@ import javax.persistence.TemporalType;
 public class ModemMessage extends BasicEntity
         implements Serializable {
 
-    protected String text;
+    @Column(name = "MGCNTC", nullable = false)
+    private String contact;
+
+    @Column(name = "MGTEXT", nullable = false)
+    private String text;
 
     /**
      * @return the text
      */
-    @Column(name = "MGTEXT", nullable = false)
     public String getText() {
         return text;
     }
