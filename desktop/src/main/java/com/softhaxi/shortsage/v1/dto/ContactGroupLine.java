a442bfa9c2ad70de00ab84a5c03e2703e41cd717
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-04-20 16:10:35 +0700

    Add Field contact

--- desktop/src/main/java/com/softhaxi/shortsage/v1/dto/ContactGroupLine.java
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/dto/ContactGroupLine.java
@@ -18,199 +18,30 @@ import org.hibernate.annotations.GenericGenerator;
  */
 @Entity
 @Table(name = "D0GRLN")
-public class ContactGroupLine 
+@AttributeOverrides({
+    @AttributeOverride(name = "id",
+            column = @Column(name = "GGGLID", unique = true)),
+    @AttributeOverride(name = "name",
+            column = @Column(name = "GLGLNA", length = 100)),
+    @AttributeOverride(name = "remark",
+            column = @Column(name = "GLRMRK")),
+    @AttributeOverride(name = "status",
+            column = @Column(name = "GLRCST")),
+    @AttributeOverride(name = "createdBy",
+            column = @Column(name = "GLCRBY")),
+    @AttributeOverride(name = "createdDate",
+            column = @Column(name = "GLCRDT")),
+    @AttributeOverride(name = "modifiedBy",
+            column = @Column(name = "GLMFBY")),
+    @AttributeOverride(name = "modifiedDate",
+            column = @Column(name = "CLMFDT")),
+    @AttributeOverride(name = "deletedState",
+            column = @Column(name = "CLDLST")),
+    @AttributeOverride(name = "version",
+            column = @Column(name = "CLVRSN"))
+})
+public class ContactGroupLine extends BasicEntity
     implements Serializable {
-    
-    @Id
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    @Column(name = "GLGLID", unique = true)
-    private String id;
-    
-    @Column(name = "GLCGID", nullable = false)
-    private String group;
-    
-    @Column(name = "GLCPID", nullable = false)
-    private String person;
-    
-    @Column(name = "GLRMRK")
-    private String remark;
-    
-    @Column(name = "GLRCTS")
-    private int status;
-
-    @Column(name = "GLCRBY", length = 100)
-    private String createdBy;
-
-    @Column(name = "GLCRON")
-    private Date createdOn;
-
-    @Column(name = "GLMDBY", length = 100)
-    private String modifiedBy;
-
-    @Column(name = "GLMDON")
-    private Date modifiedOn;
-
-    @Column(name = "GLDLST")
-    private int deletedState;
-
-    @Version
-    @Column(name = "GLVRSN")
-    private Integer version;
-
-    /**
-     * @return the id
-     */
-    public String getId() {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    /**
-     * @return the group
-     */
-    public String getGroup() {
-        return group;
-    }
-
-    /**
-     * @param group the group to set
-     */
-    public void setGroup(String group) {
-        this.group = group;
-    }
-
-    /**
-     * @return the person
-     */
-    public String getPerson() {
-        return person;
-    }
-
-    /**
-     * @param person the person to set
-     */
-    public void setPerson(String person) {
-        this.person = person;
-    }
-
-    /**
-     * @return the remark
-     */
-    public String getRemark() {
-        return remark;
-    }
-
-    /**
-     * @param remark the remark to set
-     */
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    /**
-     * @return the status
-     */
-    public int getStatus() {
-        return status;
-    }
-
-    /**
-     * @param status the status to set
-     */
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    /**
-     * @return the createdBy
-     */
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    /**
-     * @param createdBy the createdBy to set
-     */
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    /**
-     * @return the createdOn
-     */
-    public Date getCreatedOn() {
-        return createdOn;
-    }
-
-    /**
-     * @param createdOn the createdOn to set
-     */
-    public void setCreatedOn(Date createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    /**
-     * @return the modifiedBy
-     */
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
-
-    /**
-     * @param modifiedBy the modifiedBy to set
-     */
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
-
-    /**
-     * @return the modifiedOn
-     */
-    public Date getModifiedOn() {
-        return modifiedOn;
-    }
-
-    /**
-     * @param modifiedOn the modifiedOn to set
-     */
-    public void setModifiedOn(Date modifiedOn) {
-        this.modifiedOn = modifiedOn;
-    }
-
-    /**
-     * @return the deletedState
-     */
-    public int getDeletedState() {
-        return deletedState;
-    }
-
-    /**
-     * @param deletedState the deletedState to set
-     */
-    public void setDeletedState(int deletedState) {
-        this.deletedState = deletedState;
-    }
-
-    /**
-     * @return the version
-     */
-    public Integer getVersion() {
-        return version;
-    }
-
-    /**
-     * @param version the version to set
-     */
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-    
-    
+    @Column(name = "CLCCID")
+    private String contact;
 }
