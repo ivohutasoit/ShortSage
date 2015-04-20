46f25400393d74ed9afe974723db76e0070a2ccb
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2015-04-20 13:17:12 +0700

    Update Contact

--- desktop/src/main/java/com/softhaxi/shortsage/v1/dto/Contact.java
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/dto/Contact.java
@@ -24,180 +24,30 @@ import org.hibernate.annotations.GenericGenerator;
 @Table(name = "M0CNTC")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "CCCCTY", discriminatorType = DiscriminatorType.STRING)
-public class Contact implements Serializable {
-    @Id
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    @Column(name = "CCCCID", unique = true)
-    protected String id;
-    
-    @Column(name = "CCCCNA", length = 100)
-    protected String name;
-    
-    @Column(name = "CCRMRK")
-    protected String remark;
-    
-    @Column(name = "CCRCTS")
-    protected int status;
-
-    @Column(name = "CCCRBY", length = 100)
-    protected String createdBy;
-
-    @Column(name = "CCCRON")
-    protected Date createdOn;
-
-    @Column(name = "CCMDBY", length = 100)
-    protected String modifiedBy;
-
-    @Column(name = "CCMDON")
-    protected Date modifiedOn;
-
-    @Column(name = "CCDLST")
-    protected int deletedState;
-
-    @Version
-    @Column(name = "CCVRSN")
-    protected Integer version;
-
-    /**
-     * @return the id
-     */
-    public String getId() {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return the remark
-     */
-    public String getRemark() {
-        return remark;
-    }
-
-    /**
-     * @param remark the remark to set
-     */
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    /**
-     * @return the status
-     */
-    public int getStatus() {
-        return status;
-    }
-
-    /**
-     * @param status the status to set
-     */
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    /**
-     * @return the createdBy
-     */
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    /**
-     * @param createdBy the createdBy to set
-     */
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    /**
-     * @return the createdOn
-     */
-    public Date getCreatedOn() {
-        return createdOn;
-    }
-
-    /**
-     * @param createdOn the createdOn to set
-     */
-    public void setCreatedOn(Date createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    /**
-     * @return the modifiedBy
-     */
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
-
-    /**
-     * @param modifiedBy the modifiedBy to set
-     */
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
-
-    /**
-     * @return the modifiedOn
-     */
-    public Date getModifiedOn() {
-        return modifiedOn;
-    }
-
-    /**
-     * @param modifiedOn the modifiedOn to set
-     */
-    public void setModifiedOn(Date modifiedOn) {
-        this.modifiedOn = modifiedOn;
-    }
-
-    /**
-     * @return the deletedState
-     */
-    public int getDeletedState() {
-        return deletedState;
-    }
-
-    /**
-     * @param deletedState the deletedState to set
-     */
-    public void setDeletedState(int deletedState) {
-        this.deletedState = deletedState;
-    }
-
-    /**
-     * @return the version
-     */
-    public Integer getVersion() {
-        return version;
-    }
-
-    /**
-     * @param version the version to set
-     */
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-    
+@DiscriminatorValue(value = "MSSG")
+@AttributeOverrides({
+    @AttributeOverride(name = "id",
+            column = @Column(name = "CCCCID", unique = true)),
+    @AttributeOverride(name = "name",
+            column = @Column(name = "CCCCNA", length = 100)),
+    @AttributeOverride(name = "remark",
+            column = @Column(name = "CCRMRK")),
+    @AttributeOverride(name = "status",
+            column = @Column(name = "CCRCST")),
+    @AttributeOverride(name = "createdBy",
+            column = @Column(name = "CCCRBY")),
+    @AttributeOverride(name = "createdDate",
+            column = @Column(name = "CCCRDT")),
+    @AttributeOverride(name = "modifiedBy",
+            column = @Column(name = "CCMFBY")),
+    @AttributeOverride(name = "modifiedDate",
+            column = @Column(name = "CCMFDT")),
+    @AttributeOverride(name = "deletedState",
+            column = @Column(name = "CCDLST")),
+    @AttributeOverride(name = "version",
+            column = @Column(name = "CCVRSN"))
+})
+public class Contact extends BasicEntity
+        implements Serializable {
     
 }
