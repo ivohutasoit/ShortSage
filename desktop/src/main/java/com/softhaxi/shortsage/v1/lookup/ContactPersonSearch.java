df16a8cbc244fb88c31b2904bb9af73afa463f12
Ivo Hutasoit <raja.yudhaps@gmail.com>
2015-04-23 00:48:12 +0700

    Fix Some Classes

---
+++ desktop/src/main/java/com/softhaxi/shortsage/v1/lookup/ContactPersonSearch.java
@@ -0,0 +1,32 @@
+package com.softhaxi.shortsage.v1.lookup;
+
+import java.util.ResourceBundle;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import org.jdesktop.swingx.JXTable;
+
+public class ContactPersonSearch extends JPanel {
+
+    private static final ResourceBundle RES_GLOBAL = ResourceBundle.getBundle("global");
+
+    private static final String[] COLUMN_NAMES = new String[]{
+        ""
+    };
+
+    private JXTable ttData;
+    private JButton bOK, bCancel, bClear;
+
+    public ContactPersonSearch() {
+
+        initComponents();
+        initListeners();
+    }
+
+    private void initComponents() {
+
+    }
+
+    private void initListeners() {
+
+    }
+}
